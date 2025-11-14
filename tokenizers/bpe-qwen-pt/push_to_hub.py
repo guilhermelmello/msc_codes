9f97e826ebb30c8bc15c16d87a18b7e4e9e6c832
Guilherme Lamartine de Mello <guilhermelmello@yahoo.com.br>
2025-11-14 08:02:07 -0300

    Add new tokenizer models.

--- tokenizers/bpe-qwen-pt/push_to_hub.py
+++ tokenizers/bpe-qwen-pt/push_to_hub.py
@@ -3,9 +3,14 @@ import huggingface_hub
 from transformers import AutoTokenizer
 
 model_names = [
+    ('bpe5k',  'tokenizer-bpe-pt-5k'),
     ('bpe10k', 'tokenizer-bpe-pt-10k'),
     ('bpe30k', 'tokenizer-bpe-pt-30k'),
     ('bpe50k', 'tokenizer-bpe-pt-50k'),
+    ('unigram5k', 'tokenizer-unigram-pt-5k'),
+    ('unigram10k', 'tokenizer-unigram-pt-10k'),
+    ('unigram30k', 'tokenizer-unigram-pt-30k'),
+    ('unigram50k', 'tokenizer-unigram-pt-50k'),
 ]
 
 huggingface_hub.login(os.environ['HF_TOKEN'])
@@ -18,4 +23,5 @@ for local_name, repo_id in model_names:
     print('Hub ID:', repo_id)
 
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-    tokenizer.push_to_hub(repo_id, private=True)
+    tokenizer.push_to_hub(repo_id, private=False)
+
