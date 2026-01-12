ff293034a076a662bf9c488a27bc7fd056ad7afc
Guilherme Lamartine de Mello <guilhermelmello@yahoo.com.br>
2026-01-12 05:33:55 -0300

    Update script.

--- tokenizers/qwen-pt/push_to_hub.py
+++ tokenizers/qwen-pt/push_to_hub.py
@@ -3,17 +3,21 @@ import huggingface_hub
 from transformers import AutoTokenizer
 
 model_names = [
-    ('bpe5k',  'tokenizer-bpe-pt-5k'),
-    ('bpe10k', 'tokenizer-bpe-pt-10k'),
-    ('bpe30k', 'tokenizer-bpe-pt-30k'),
-    ('bpe50k', 'tokenizer-bpe-pt-50k'),
-    ('unigram5k', 'tokenizer-unigram-pt-5k'),
-    ('unigram10k', 'tokenizer-unigram-pt-10k'),
-    ('unigram30k', 'tokenizer-unigram-pt-30k'),
-    ('unigram50k', 'tokenizer-unigram-pt-50k'),
+    ('bpe5k-nfc',  'tokenizer-bpe-pt-5k'),
+    ('bpe8k-nfc',  'tokenizer-bpe-pt-8k'),
+    ('bpe10k-nfc', 'tokenizer-bpe-pt-10k'),
+    ('bpe15k-nfc', 'tokenizer-bpe-pt-15k'),
+    ('bpe30k-nfc', 'tokenizer-bpe-pt-30k'),
+    ('bpe50k-nfc', 'tokenizer-bpe-pt-50k'),
+    ('unigram5k-nfc', 'tokenizer-unigram-pt-5k'),
+    ('unigram8k-nfc', 'tokenizer-unigram-pt-8k'),
+    ('unigram10k-nfc', 'tokenizer-unigram-pt-10k'),
+    ('unigram15k-nfc', 'tokenizer-unigram-pt-15k'),
+    ('unigram30k-nfc', 'tokenizer-unigram-pt-30k'),
+    ('unigram50k-nfc', 'tokenizer-unigram-pt-50k'),
 ]
 
-huggingface_hub.login(os.environ['HF_TOKEN'])
+huggingface_hub.login()
 
 for local_name, repo_id in model_names:
     tokenizer_path = os.path.join('models', local_name)
@@ -22,6 +26,5 @@ for local_name, repo_id in model_names:
     print('Local Path:', tokenizer_path)
     print('Hub ID:', repo_id)
 
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-    tokenizer.push_to_hub(repo_id, private=False)
-
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, padding_side='left')
+    tokenizer.push_to_hub(f'guilhermelmello/{repo_id}')
