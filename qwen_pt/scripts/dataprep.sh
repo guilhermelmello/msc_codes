edda650070ede5d3118c17b72e0581f71c9c712c
Guilherme Lamartine de Mello <guilhermelmello@yahoo.com.br>
2026-02-17 09:05:00 -0300

    Add Qwen3-0.6B dataprep.

--- qwen_pt/scripts/dataprep.sh
+++ qwen_pt/scripts/dataprep.sh
@@ -2,8 +2,8 @@
 #PBS -N dataprep
 #PBS -q testes
 #PBS -l nodes=1:ppn=4
-#PBS -e logs/dataprep.err
-#PBS -o logs/dataprep.out
+#PBS -e logs/dataprep_qwen3.err
+#PBS -o logs/dataprep_qwen3.out
 
 NUM_PROC=4
 
@@ -21,24 +21,32 @@ export HF_EVALUATE_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
 
 
-echo "Running Unigram Tokenizer"
-python src/dataprep.py \
-    --save-path datasets/clm-1024-unigram-pt-8k \
-    --tokenizer-name guilhermelmello/tokenizer-unigram-pt-8k \
-    --max-seq-length 1024 \
-    --batch-size 256 \
-    --num-proc $NUM_PROC \
-    --seed 42
+# echo "Running Unigram Tokenizer"
+# python src/dataprep.py \
+#     --save-path datasets/clm-1024-unigram-pt-8k \
+#     --tokenizer-name guilhermelmello/tokenizer-unigram-pt-8k \
+#     --max-seq-length 1024 \
+#     --batch-size 256 \
+#     --num-proc $NUM_PROC \
+#     --seed 42
+
+# echo "Running BPE Tokenizer"
+# python src/dataprep.py \
+#     --save-path datasets/clm-1024-bpe-pt-8k \
+#     --tokenizer-name guilhermelmello/tokenizer-bpe-pt-8k \
+#     --max-seq-length 1024 \
+#     --batch-size 512 \
+#     --num-proc $NUM_PROC \
+#     --seed 42
 
 echo "Running BPE Tokenizer"
 python src/dataprep.py \
-    --save-path datasets/clm-1024-bpe-pt-8k \
-    --tokenizer-name guilhermelmello/tokenizer-bpe-pt-8k \
+    --save-path datasets/qwen3-06b \
+    --tokenizer-name Qwen/Qwen3-0.6B \
     --max-seq-length 1024 \
     --batch-size 512 \
     --num-proc $NUM_PROC \
     --seed 42
-
 
 deactivate
 echo "Ending Time: $(date)"
