09898f7260ed4c14e0680e8fad019bb4f0710db3
Guilherme Lamartine de Mello <guilhermelmello@yahoo.com.br>
2026-07-24 06:02:44 -0300

    Update results

--- finetuning/scripts/assin2-rte.sh
+++ finetuning/scripts/assin2-rte.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -N a2-rte
 #PBS -q miggpu24h
-#PBS -e logs/assin2-rte/bertimbau-large.err
-#PBS -o logs/assin2-rte/bertimbau-large.out
+#PBS -e logs/assin2-rte/albertina-100m/seed-43.err
+#PBS -o logs/assin2-rte/albertina-100m/seed-43.out
 
 echo "Staring Time: $(date)"
 echo "Root directory $PBS_O_WORKDIR"
@@ -25,12 +25,14 @@ export TRANSFORMERS_OFFLINE=1
 echo "Running python script"
 python main.py \
     --task-name assin2-rte \
-    --model-name neuralmind/bert-large-portuguese-cased \
-    --save-dir models/assin2-rte/bertimbau-large \
+    --model-name PORTULAN/albertina-100m-portuguese-ptbr-encoder \
+    --save-dir models/assin2-rte/albertina-100m/43 \
+    --hp-learning-rate 5e-3 5e-4 5e-5 5e-6 \
+    --hp-batch-size 8 16 32 \
     --num-hp-trials 12 \
     --num-hp-epochs 5 \
     --num-training-epochs 10 \
-    --seed 42
+    --seed 43
 
 
 deactivate
